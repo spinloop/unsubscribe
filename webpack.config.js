db4bcbabebb75c0dca41ab6b86aaae9d14b25cd3
Chris Eldridge <cre@mojotech.com>
2016-09-26 11:11:05 -0400

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+
+const PATHS = {
+  app: path.resolve('src'),
+  build: path.resolve('build'),
+};
+
+module.exports = {
+  entry: {
+    app: `${PATHS.app}/app.jsx`,
+  },
+
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: PATHS.app,
+      },
+    ],
+  },
+};
