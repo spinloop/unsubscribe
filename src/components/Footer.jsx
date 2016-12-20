4e5962c2829ada3bdc955350b8fefc823bc0fade
Chris Eldridge <cre@mojotech.com>
2016-12-20 10:07:18 -0500

    update social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <div className="logo">ROSS NEWKIRK</div>
     <div className="social-media">
       <div className="facebook">
-        <a href="https://www.facebook.com/" target="_blank">
+        <a href="https://www.facebook.com/unsubscribebook" target="_blank">
           <img
             src="https://s3-us-west-2.amazonaws.com/unsubscribe-book/social-icons/fb.png"
             alt="facebook"
@@ -13,18 +13,10 @@ const Footer = () => (
         </a>
       </div>
       <div className="twitter">
-        <a href="https://twitter.com/" target="_blank">
+        <a href="https://twitter.com/Unsubscribebook" target="_blank">
           <img
             src="https://s3-us-west-2.amazonaws.com/unsubscribe-book/social-icons/twitter.png"
             alt="twitter"
-          />
-        </a>
-      </div>
-      <div className="linkedin">
-        <a href="https://www.linkedin.com/" target="_blank">
-          <img
-            src="https://s3-us-west-2.amazonaws.com/unsubscribe-book/social-icons/linkedin.png"
-            alt="linkedin"
           />
         </a>
       </div>
