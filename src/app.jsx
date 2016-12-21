fc37fbbd5783b6bea7191b6c13800aa7259751be
Chris Eldridge <cre@mojotech.com>
2016-12-21 07:09:01 -0500

    refactor form

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Slideshow from './components/Slideshow.jsx';
-import Form from './components/Form.jsx';
+import FormContainer from './components/FormContainer.jsx';
 import Description from './components/Description.jsx';
 import Hero from './components/Hero.jsx';
 import Bio from './components/Bio.jsx';
@@ -20,15 +20,7 @@ const App = () => (
       <Description />
       <Slideshow />
       <Inspiration />
-      <section id="form-section">
-        <header className="section-header">
-          <h2>Share how you have been Unsubscribing!</h2>
-        </header>
-        <div className="sub-header">
-          <p>You will also receive updates on this and other upcoming books!</p>
-        </div>
-        <Form />
-      </section>
+      <FormContainer />
       <Bio />
     </div>
     <Footer />
