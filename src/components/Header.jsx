831fd10003600492fdfb3546f775f4cef7e7ba75
Chris Eldridge <cre@mojotech.com>
2016-12-23 21:13:56 -0500

    replace sign-up link with amazon buy-now link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,13 @@ const Header = () => (
     <div className="logo">ROSS NEWKIRK</div>
     <nav>
       <a className="learn-more" href="#description">Learn More</a>
-      <a className="sign-up" href="#form-section">Sign-up</a>
+      <a
+        className="buy-now"
+        href="https://www.amazon.com/dp/1940129605/ref=sr_1_11?ie=UTF8&qid=1482080344&sr=8-11&keywords=unsubscribe"
+        target="_blank"
+      >
+        Buy Now
+      </a>
     </nav>
   </header>
 );
